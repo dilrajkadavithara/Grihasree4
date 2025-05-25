3c68a3b2d1759b28ba8b76e4d3b52aca35234ae0
dilrajkadavithara <dilrajkadavithara@gmail.com>
2025-05-25 20:25:13 +0530

    Verified settings.py for Railway deployment

--- grihasree_project/settings.py
+++ grihasree_project/settings.py
@@ -22,7 +22,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR, '.env'))  # Loads your .env file
+# load_dotenv(os.path.join(BASE_DIR, '.env'))  # Loads your .env file
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
