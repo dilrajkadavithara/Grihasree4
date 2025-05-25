04d140a16e9523d76913fed7043766d1b1db0d6c
dilrajkadavithara <dilrajkadavithara@gmail.com>
2025-05-25 18:32:11 +0530

    Fix: Use dj-database-url for DATABASES config

--- grihasree_project/settings.py
+++ grihasree_project/settings.py
@@ -99,7 +99,11 @@ WSGI_APPLICATION = 'grihasree_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        default=f"mysql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}",
+        conn_max_age=600,
+        ssl_require=False,
+    )
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
